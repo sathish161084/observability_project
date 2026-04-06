f3ac1fb60d888bfeace59143cf3ec81eca24f696
sathish161084 <sathish.161084@gmail.com>
2026-04-06 15:35:37 +0100

    update import in backend py file

--- app-src/backend/app/main.py
+++ app-src/backend/app/main.py
@@ -16,7 +16,7 @@ from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
-from opentelemetry.sdk.resources import RESOURCE_ATTRIBUTES, SERVICE_NAME, Resource
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
